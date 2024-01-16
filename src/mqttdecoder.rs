7dfc2fb2739fd86c91a1b197c4db937f7e8bfe31
heya-naohiro <Naohiro.Heya@sony.com>
2024-01-16 23:45:21 +0900

    impl: publish decoder

--- src/mqttdecoder.rs
+++ src/mqttdecoder.rs
@@ -2,6 +2,7 @@ use bytes::Buf;
 use bytes::BufMut;
 use bytes::BytesMut;
 use std::io::{Error, ErrorKind};
+use std::str;
 use tokio_util::codec::{Decoder, Encoder};
 #[derive(Debug)]
 pub enum MQTTPacket {
@@ -46,17 +47,48 @@ impl Connack {
 }
 
 #[derive(Debug)]
-pub struct Publish {}
+pub struct Publish {
+    topic_name: String,
+    message_id: u32,
+}
 
-impl Publish {}
+impl Publish {
+    pub fn from_byte(buf: &mut BytesMut) -> Result<Option<(Publish, usize)>, Error> {
+        // topic length : 2 byte + Message Identification length: 2byte
+        if buf.len() < 4 {
+            return Ok(None);
+        }
+        let topic_length: usize = ((buf[0] as usize) << 8) + buf[1] as usize;
+        if buf.len() < topic_length + 4 {
+            return Ok(None);
+        }
+        let slice = &buf[2..(2 + topic_length)];
+        let topic_name = match std::str::from_utf8(slice) {
+            Ok(v) => v,
+            Err(e) => {
+                return Err(Error::new(ErrorKind::Other, "Invalid"));
+            }
+        };
+        let message_id: u32 = ((buf[2 + topic_length] as u32) << 8) + buf[3 + topic_length] as u32;
+
+        // [TODO]
+        return Ok(Some((
+            Publish {
+                topic_name: String::from(topic_name),
+                message_id,
+            },
+            4 + topic_length,
+        )));
+    }
+}
 
 pub struct MqttDecoder {
-    header: bool,
+    header: Option<Header>,
 }
 
 impl MqttDecoder {
     pub fn new() -> MqttDecoder {
-        MqttDecoder { header: true }
+        MqttDecoder { header: None }
     }
 }
 
@@ -75,6 +107,7 @@ fn read_header(src: &mut BytesMut) -> Result<Option<(Header, usize)>, Error> {
         return Ok(None);
     } else {
         let byte = src[0];
+        let mut advance = 1;
         let dup = byte & 0b00001000 == 0b00001000;
         let qos = (byte & 0b00000110) >> 1;
         let retain = byte & 0b00000001 == 0b00000110;
@@ -82,6 +115,7 @@ fn read_header(src: &mut BytesMut) -> Result<Option<(Header, usize)>, Error> {
         // "残りの長さ"の箇所は最大4つ
         for pos in 0..=3 {
             let byte = src[pos + 1];
+            advance += 1;
             remaining_length += (byte as usize & 0b0111111) << (pos * 7);
             if (byte & 0b10000000) == 0 {
                 break;
@@ -106,7 +140,7 @@ fn read_header(src: &mut BytesMut) -> Result<Option<(Header, usize)>, Error> {
                 retain,
                 remaining_length,
             },
-            0,
+            advance,
         )));
     }
 }
@@ -115,25 +149,44 @@ impl Decoder for MqttDecoder {
     type Item = MQTTPacket;
     type Error = std::io::Error;
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        if self.header {
-            let length = src.len();
-            if src.len() < 2 {
-                return Ok(None);
-            }
-            let byte = src[0];
+        match self.header {
+            None => {
+                let length = src.len();
+                if src.len() < 2 {
+                    return Ok(None);
+                }
 
-            match byte >> 4 {
-                1 => {
-                    src.advance(length);
-                    Ok(Some(MQTTPacket::Connect))
-                }
-                _ => {
-                    src.advance(length);
-                    Err(Error::new(ErrorKind::Other, "Invalid"))
+                // 固定ヘッダーに可変長(残りの長さを含むため固定ヘッダーを解読できたら読んだreadbyte分進める必要がある)
+                let (header, readbyte) = match read_header(src) {
+                    Ok(Some(value)) => value,
+                    Ok(None) => return Ok(None),
+                    Err(e) => return Err(e),
+                };
+
+                self.header = Some(header);
+                src.advance(readbyte);
+                match Some(self.header) {
+                    MQTTPacketHeader::Connect => Ok(Some(MQTTPacket::Connect)),
+                    MQTTPacketHeader::Publish => {
+                        let (variable_header_only, readbyte) = match Publish::from_byte(src) {
+                            Ok(Some(value)) => value,
+                            Ok(None) => return Ok(None),
+                            Err(e) => return Err(e),
+                        };
+                        src.advance(readbyte);
+                        // TODO: add payload processing
+                        Ok(Some(MQTTPacket::Publish(variable_header_only)))
+                    }
+                    _ => {
+                        src.advance(length);
+                        Err(Error::new(ErrorKind::Other, "Invalid"))
+                    }
                 }
             }
-        } else {
-            Err(Error::new(ErrorKind::Other, "Not Implemented"))
+            Some(header) => match header.mtype {
+                // [TODO] second packet implement
+                _ => Err(Error::new(ErrorKind::Other, "Invalid")),
+            },
         }
     }
 }
