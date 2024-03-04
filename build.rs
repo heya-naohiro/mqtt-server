433a06cc12ebc88dcb7c51903fd225e8411a0785
heya-naohiro <Naohiro.Heya@sony.com>
2024-03-05 01:47:30 +0900

    fix: pipeline

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() {
-    tonic_build::compile_protos("proto/published_packet.proto")
+    let mut config = prost_build::Config::new();
+    config.protoc_arg("--experimental_allow_proto3_optional");
+    tonic_build::configure()
+        .compile_with_config(config, &["proto/published_packet.proto"], &["proto"])
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
 }
