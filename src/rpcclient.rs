44c6102bbe4ccad26eba73ad00a2fda19990153e
heya-naohiro <Naohiro.Heya@sony.com>
2024-03-17 18:36:11 +0900

    tarf cmd add sub for test

--- src/rpcclient.rs
+++ src/rpcclient.rs
@@ -1,5 +1,6 @@
+use rand::distributions::{Alphanumeric, DistString};
 use rpcserver::published_packet_service_client::PublishedPacketServiceClient;
-use rpcserver::PublishedPacketRequest;
+use rpcserver::{PublishRequest, PublishedPacketRequest};
 use std::error::Error;
 use tonic::transport::Channel;
 use tonic::Request;
@@ -19,6 +20,25 @@ pub async fn print_published_payloads(
     while let Some(packet) = stream.message().await? {
         println!("Packet = {:?}", packet);
     }
+
+    Ok(())
+}
+
+pub async fn publish_random_payloads(
+    client: &mut PublishedPacketServiceClient<Channel>,
+) -> Result<(), Box<dyn Error>> {
+    //let mut rng = rand::thread_rng();
+    //let random_code = Alphanumeric.sample_string(&mut rng, 32);
+    let payload: String = String::from("hello payload world😀"); // + &random_code;
+                                                                 //let payload = format!("{}{}", payload, random_code);
+    let request = PublishRequest {
+        device_id: "test_device".to_string(),
+        topic: "hello/world".to_string(),
+        payload: payload.into_bytes(),
+    };
+    if let Err(err) = client.publish_payload_to_device(request).await {
+        println!("Publish Error {:?}", err)
+    };
 
     Ok(())
 }
