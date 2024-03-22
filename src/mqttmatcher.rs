69fa6f0fbab423ba85d7d16077aedfddfdfeed39
heya-naohiro <Naohiro.Heya@sony.com>
2024-03-22 12:01:48 +0900

    add: topic filter trait

--- src/mqttmatcher.rs
+++ src/mqttmatcher.rs
@@ -1,39 +1,75 @@
 use itertools::Itertools;
 
-struct TopicFilter {
-    elements: Vec<Vec<String>>,
+struct TopicFilterStore {
+    elements: Vec<Box<dyn TopicFilter>>,
 }
 
-/*
-// extra infomartion for efficiency
-struct Element {
-    topicelement: String,
-    plus_include: bool,
-    sharp_include: bool,
+pub struct SubInfo {
+    topicfilter_elements: Vec<String>,
 }
-*/
 
-impl TopicFilter {
-    pub fn new() -> Self {
-        return TopicFilter { elements: vec![] };
+impl TopicFilter for SubInfo {
+    fn get_topic_filter(&self) -> &Vec<String> {
+        return &self.topicfilter_elements;
     }
-    pub fn register_topicfilter(&mut self, topicfilter: String) -> std::io::Result<()> {
-        if topicfilter.split('/').any(|s| !self.valid(s)) {
+}
+
+impl SubInfo {
+    fn new(topicfilter: String) -> Self {
+        SubInfo {
+            topicfilter_elements: topicfilter.split('/').map(|s| s.to_string()).collect(),
+        }
+    }
+}
+
+pub trait TopicFilter {
+    fn get_topic_filter(&self) -> &Vec<String>;
+}
+
+impl TopicFilterStore {
+    pub fn new() -> Self {
+        return TopicFilterStore { elements: vec![] };
+    }
+    /*
+        pub fn register_topicfilter(&mut self, topicfilter: String) -> std::io::Result<()> {
+            if topicfilter.split('/').any(|s| !self.valid(s)) {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Invalid Topicfilter",
+                ));
+            }
+
+            let p: Vec<String> = topicfilter.split('/').map(|s| s.to_string()).collect();
+            self.elements.push(p);
+            Ok(())
+        }
+    */
+    pub fn register_topicfilter<T: TopicFilter + 'static>(
+        &mut self,
+        topicfilter: T,
+    ) -> std::io::Result<()> {
+        if topicfilter
+            .get_topic_filter()
+            .iter()
+            .any(|s| !self.valid(s))
+        {
             return Err(std::io::Error::new(
                 std::io::ErrorKind::Other,
                 "Invalid Topicfilter",
             ));
         }
-
-        let p: Vec<String> = topicfilter.split('/').map(|s| s.to_string()).collect();
-        self.elements.push(p);
+        self.elements.push(Box::new(topicfilter));
         Ok(())
     }
 
     pub fn topic_match(&mut self, topic: String) -> std::io::Result<bool> {
         for topic_filter in self.elements.iter() {
             let mut matched = true;
-            for eob in topic.split('/').into_iter().zip_longest(topic_filter) {
+            for eob in topic
+                .split('/')
+                .into_iter()
+                .zip_longest(topic_filter.get_topic_filter())
+            {
                 match eob {
                     itertools::EitherOrBoth::Both(te, tfe) => {
                         if tfe == "#" {
@@ -86,7 +122,7 @@ mod tests {
 
     #[test]
     fn test_check_element() {
-        let m = TopicFilter::new();
+        let m = TopicFilterStore::new();
         assert_eq!(m.valid("helloworld"), true);
         assert_eq!(m.valid("hello-world"), true);
         assert_eq!(m.valid("hello-worl.d"), true);
@@ -104,8 +140,10 @@ mod tests {
 
     #[test]
     fn test_aws_example_1() {
-        let mut m = TopicFilter::new();
-        let _ = m.register_topicfilter("sensor/#".to_string()).unwrap();
+        let mut m = TopicFilterStore::new();
+        let _ = m
+            .register_topicfilter(SubInfo::new("sensor/#".to_string()))
+            .unwrap();
         assert_eq!(m.topic_match("sensor/".to_string()).unwrap(), true);
         assert_eq!(
             m.topic_match("sensor/temperature".to_string()).unwrap(),
@@ -120,9 +158,9 @@ mod tests {
     }
     #[test]
     fn test_aws_example_2() {
-        let mut m = TopicFilter::new();
+        let mut m = TopicFilterStore::new();
         let _ = m
-            .register_topicfilter("sensor/+/room1".to_string())
+            .register_topicfilter(SubInfo::new("sensor/+/room1".to_string()))
             .unwrap();
         assert_eq!(
             m.topic_match("sensor/temperature/room1".to_string())
